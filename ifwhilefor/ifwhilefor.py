a2d9775602245bf8881bf2a48951729ff5a30446
Harry Oslislo <oslislo1@hotmail.com.com>
2017-07-12 07:32:11 -0400

    initial push to github

---
+++ ifwhilefor/ifwhilefor.py
@@ -0,0 +1,20 @@
+# if while for
+print "if --"
+x = 1
+if x > 0:
+    print("yo")
+else:
+    print("dude")
+
+print "while --"
+x = 1
+while x >= 0:
+    print("yo" if x >= 0 else "dude")
+    #x *= -1
+    x += -1
+
+print "for --"
+for x in range(3, 8, 2):
+    print(x)
+
+print "done"
